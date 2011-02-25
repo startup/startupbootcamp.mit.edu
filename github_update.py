4bcdce245bd24a418b87b3c1cc9dd5d8e152b484
Michael Grinich <grinich@mit.edu>
2011-02-25 02:21:25 -0800

    make it cgi

--- github_update.py
+++ github_update.py
@@ -1,2 +1,34 @@
 import os
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
+
+print "Content-type: text/html"
+print
+
+print """
+<html>
+
+<head><title>Success</title></head>
+
+<body>
+
+  <h3> Success </h3>
+"""
+
 os.system("git pull")
+
+print """
+
+  <p>Previous message: %s</p>
+
+  <p>form
+
+  <form method="post" action="index.cgi">
+    <p>message: <input type="text" name="message"/></p>
+  </form>
+
+</body>
+
+</html>
+""" % cgi.escape(message)
+
